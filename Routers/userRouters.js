28d3ad4a36710cf275ad2b1c0fbb27fc6408b443
shivamkumarsingh98 <shiavmkumarsingh.chu98@gmail.com>
2024-04-08 11:36:03 +0530

    fix issue registercontroler

--- Routers/userRouters.js
+++ Routers/userRouters.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const register = require("../Controler/registerControler");
-const Login = require(".././Controler/loginControler");
+const register = require("../Controler/registerControler.js");
+const Login = require(".././Controler/loginControler.js");
 const userRoute = express.Router();
 
 userRoute.use(express.json());
