9dfe95e6905ab48c0f201514245e6c08d893657d
shivamkumarsingh98 <shiavmkumarsingh.chu98@gmail.com>
2024-04-12 19:02:44 +0530

    complite

--- Utils/Cloudinary.js
+++ Utils/Cloudinary.js
@@ -20,7 +20,7 @@ const uploadOnCloudinary = async (localFilePath) => {
 
     return response;
   } catch (error) {
-    // fs.unlinkSync(localFilePath);
+    fs.unlinkSync(localFilePath);
     console.log("errorClodniry", error);
   } finally {
     fs.unlinkSync(localFilePath);
--- controler/paymentReciveControler.js
+++ controler/paymentReciveControler.js
@@ -5,9 +5,11 @@ const { uploadOnCloudinary } = require("../Utils/Cloudinary");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    return cb(null, "./public/images");
+    console.log("Destination function called");
+    return cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
+    console.log("Filename function called");
     return cb(null, file.originalname);
   },
 });
